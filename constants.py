b8560648912efb08f19f1700d17a43dd93c72e0f
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-21 14:45:26 +0530

    created constants.py fro constants like hiwayUrl and driver path

---
+++ constants.py
@@ -0,0 +1,7 @@
+hiway_url = "http://qa.hiway.hashedin.com/"
+
+path_login_data_csv = "/home/jitesh_dhoot/Selenium-track/tests/login_data.csv"
+
+path_chrome_driver = "/home/jitesh_dhoot/Selenium-track/mydriver/chromedriver"
+
+path_firefox_driver = "/home/jitesh_dhoot/Selenium-track/mydriver/geckodriver"
