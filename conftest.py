5ac2a9f68559a3f8165cd420b198c760db4aa8c1
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-21 11:23:48 +0530

    created conftest.py file to run different browser from command line

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+# Command line options:
+# Example of allowing pytest to accept a command line option
+def pytest_addoption(parser):
+    parser.addoption("-B", "--browser",
+                     dest="browser",
+                     default="firefox",
+                     help="Browser. Valid options are firefox or chrome")
+
+
+# Test arguments:
+# Example of populating the argument 'browser' for a test
+@pytest.fixture
+def browser(request):
+    "pytest fixture for browser"
+    return request.config.getoption("-B")
+
