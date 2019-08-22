7f55252459ecb08bf20f80a6d3887862986ed5de
jitesh.dhoot <jitesh.dhoot@hashedin.com>
2019-08-22 11:56:04 +0530

    added comments for headleass chrome

--- Manager.py
+++ Manager.py
@@ -6,8 +6,8 @@ class Manager(object):
     def __init__(self, driver_name):
         self.driver_name = driver_name
         self.driver = None
-        if self.driver_name.lower() == 'chrome':
-            self.driver = webdriver.Chrome(constants.path_chrome_driver)
+        if self.driver_name.lower() == 'chrome':                              # chrome_options = Options()
+            self.driver = webdriver.Chrome(constants.path_chrome_driver)      # chrome_options.add_argument("--headless") , options=chrome_options
         elif self.driver_name.lower() == 'firefox':
             self.driver = webdriver.Firefox(executable_path=constants.path_firefox_driver)
 
